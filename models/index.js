07062cd658b940b60407c23fecc77f9662c6eb40
Alfredo Palacios <joseaph.1998@gmail.com>
2023-11-21 12:58:56 -0600

    exportación de modelos

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const models = {
+    usersModel : require('../models/user'),
+    tracksModel : require('../models/tracks'),
+    storagesModel : require('../models/storage')
+}
+
+module.exports = models
