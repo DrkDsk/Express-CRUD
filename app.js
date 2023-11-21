7047dfc9d36200a1556c5e17e8c9370f4bd0dc9c
Alfredo Palacios <joseaph.1998@gmail.com>
2023-11-21 12:57:08 -0600

    router implementado

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const dbConnect = require('./config/mongo')
-const { port } = require('./config/vars')
+const { port, STATIC_PATH } = require('./config/vars')
+const router = require('./routes')
 
 app.use(cors())
+app.use(express.json())
+app.use(express.static(STATIC_PATH))
+
+app.use('/api', router)
 app.listen(port, () => {
     console.log(`listening in port ${port}`)
 })
