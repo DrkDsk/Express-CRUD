e48133a2e93b7fa96cf1e29334f4a00f7561a597
Alfredo Palacios <alfredopalacios@blue.gen.in>
2023-11-28 12:40:09 -0600

    implementación de handle de logs

--- utils/handleLogger.js
+++ utils/handleLogger.js
@@ -1,13 +1,12 @@
 const {IncomingWebhook}= require('@slack/webhook')
 const {SLACK_WEBHOOK} = require('../config/vars')
-
 const slackWebhook = new IncomingWebhook(SLACK_WEBHOOK)
 
 const loggerStream = {
     write: message => {
         slackWebhook.send({
             text: message
-        }).then(r => {})
+        }).then(() => {})
     },
 };
 
